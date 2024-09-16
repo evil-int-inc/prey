bd3d6963f3ac8686ee77e843234f0a3674deb4ff
evil.int.inc <124188072+evil-int-inc@users.noreply.github.com>
2024-09-16 16:46:00 +0300

    bugfix/primitive-type (#38)

--- library/c++/Terminal__.cpp
+++ library/c++/Terminal__.cpp
@@ -25,4 +25,26 @@ class Terminal__ {
         static void print(const bool &a) {
             std::cout << a << std::endl;
         }
+            static void print(const Date__ &date) {
+                Terminal__::print(Date__::toISOString(date));
+            }
+            template<typename _V>
+            static void print(Vector__<_V> &v) {
+                std::string s = "[\n";
+                for (int i = 0; i < v.size(); ++i) {
+                    s += "  " + String__::to_string(v.get(i)) + ",\n";
+                }
+                Terminal__::print(s + "]");
+            }
+            template<typename _K, typename _V>
+            static void print(const Map__<_K, _V> &m) {
+                std::string s = "[\n";
+                for (std::pair<std::string, int> pair__ : m.m__)
+                {
+                    _K k = pair__.first;
+                    _V v = pair__.second;
+                    s += "  [" + String__::to_string(k) + ", " + String__::to_string(v) + "],\n";
+                }
+                Terminal__::print(s + "]");
+            }
 };
